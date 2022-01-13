23196d302b42dc7413017b7a3ea91a7bad7e7938
Parvathirajan Natarajan <39371408+parvathirajan@users.noreply.github.com>
2022-01-13 12:43:27 +0530

    Update Data.ts
    Shortened the Resume link

--- src/components/Data.ts
+++ src/components/Data.ts
@@ -50,7 +50,7 @@ export const basicData = {
     ],
     socialLink: {
       github: "https://github.com/parvathirajan",
-      myCV: "https://github.com/parvathirajan/parvathirajan/raw/master/Parvathirajan_Natarajan_CV.pdf",
+      myCV: "https://bit.ly/3I08jp4",
     },
     images: [
       { url: "images/about_i1.jpg" },

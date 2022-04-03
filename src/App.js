43d5f182fcf6d5c9ab4f672b9d979c823a71a288
Parvathirajan Natarajan <parvathi_rajan@hotmail.com>
2022-04-03 22:00:49 +0530

    convert about section as tsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import Header from "./components/Header";
-import About from "./components/About";
+import { About } from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";

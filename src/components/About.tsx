fca33d67483766f6834d50b164316d2216764fe6
Parvathirajan Natarajan <parvathi_rajan@hotmail.com>
2022-04-14 14:44:38 +0530

    Fixed Typo Issue

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,7 +11,6 @@ export const About = () => {
   return (
     <section id="resume-about">
       <div className="col-md-12">
-         ̰
         <h1 style={{ color: "black" }}>
           <span>{sectionNames.about}</span>
         </h1>
